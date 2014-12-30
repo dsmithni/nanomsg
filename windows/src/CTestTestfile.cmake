74ee430b9a83d31f3325292d337180ab5e4efcaa
Daniel Smith <daniel.smith@ni.com>
2014-12-29 20:04:40 -0600

    remove windows dir

--- windows/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Daniel/Documents/nanomsg/src
-# Build directory: C:/Users/Daniel/Documents/nanomsg/windows/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
